94edf6568c2f566d82189a65db5d562a6cdf6672
dpudpu <dpudpu11@gmail.com>
2020-09-29 18:32:06 +0900

    [Recoil] init to-do project

---
+++ frontend/recoil/to-do/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {RecoilRoot} from "recoil";
+
+function App() {
+    return (
+        <RecoilRoot>
+            hello recoil
+        </RecoilRoot>
+    );
+}
+
+export default App;
